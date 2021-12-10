d3bce60f75f8aa4bdaea2dbd3a1e4ab42fccd0b0
LineaFuller <lineafuller2@gmail.com>
2021-12-09 17:30:38 -0800

    app.py progress

--- Climate_app/app.py
+++ Climate_app/app.py
@@ -1,1 +1,35 @@
-import flask from Flask
+from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, func
+engine = create_engine("sqlite:///hawaii.sqlite")
+conn = engine.connect()
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return"Home Page"
+
+@app.route("/api/v1.0/precipitation")
+def precip():
+    Precipitation = session.query(Measurement.prcp, Measurement.date).\
+    filter(Measurement.date > '2016-08-23').all()
+
+    prcp_dict = {}
+    for row in Precipitation:
+        prcp_dict[row[1]]=row[0]
+        
+    return jsonify(prcp_dict)
+
+if __name__ == "__main__":
+    app.run(debug=True)
