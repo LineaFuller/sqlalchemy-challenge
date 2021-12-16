5974585a909f86aa38528919758ef3ac38b665f1
LineaFuller <lineafuller2@gmail.com>
2021-12-16 11:14:53 -0800

    finished app.py

--- Climate_app/app.py
+++ Climate_app/app.py
@@ -6,6 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, func
+from sqlalchemy.sql.expression import false
 engine = create_engine("sqlite:///hawaii.sqlite")
 conn = engine.connect()
 Base = automap_base()
@@ -38,10 +39,46 @@ def station():
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
-# def tobs():
+def tobs():
+    most_active = 'USC00519281'
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    tob = session.query(Measurement.tobs).\
+    filter(Measurement.station == most_active).\
+    filter(Measurement.date >= query_date).all()
+
+# Loop
+    tobs_list = []
+    for i in tob:
+        tobs_list.append(i[0])
+
+    
+    return jsonify(tobs_list)
 
 @app.route("/api/v1.0/<start>")
-# def describe():
+@app.route("/api/v1.0/<start>/<end>")
+def describe(start, end=False):
+    if end: 
+        calculations = session.query(func.min(Measurement.tobs), 
+                func.max(Measurement.tobs), 
+                func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+        session.close()
+        # print (calculations)
+        # spread operator: isolates tupple and activates spread 
+        return jsonify([*calculations[0]])
+
+    else:
+
+        calculations = session.query(func.min(Measurement.tobs), 
+                func.max(Measurement.tobs), 
+                func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+        session.close()
+        # print (calculations)
+        # spread operator: isolates tupple and activates spread 
+        return jsonify([*calculations[0]])
 
 if __name__ == "__main__":
     app.run(debug=True)
